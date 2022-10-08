82c6f5033e5d36a4865a8be589d6257bceab4e92
rilialov <ilialovrustem@gmail.com>
2022-10-08 18:19:35 +0300

    Added userForm and teamForm. Added methods for create and update team to TeamsController. Added "bootstrap-icons" webjar.

--- src/main/java/tgbot/web_service/controllers/TeamsController.java
+++ src/main/java/tgbot/web_service/controllers/TeamsController.java
@@ -3,7 +3,10 @@ package tgbot.web_service.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import tgbot.users.service.GetAllTeamsResponse;
+import tgbot.users.service.GetTeamResponse;
+import tgbot.users.service.TeamDTO;
 import tgbot.web_service.service.TeamClient;
 
 @Controller
@@ -21,5 +24,32 @@ public class TeamsController {
         model.addAttribute("teams", allTeams.getTeamsList());
         return "team/teamsList";
     }
+
+    @GetMapping("/createTeam")
+    public String createTeam(Model model) {
+        model.addAttribute("title", "Create Team");
+        model.addAttribute("form", "/saveNewTeam");
+        return "team/teamForm";
+    }
+
+    @PostMapping("/saveNewTeam")
+    public String saveNewTeam(String teamName, String teamColor) {
+        TeamDTO teamDTO = new TeamDTO();
+        teamDTO.setTeamName(teamName);
+        teamDTO.setTeamColor(teamColor);
+        teamClient.saveTeam(teamDTO);
+        return "redirect:/teamsList";
+    }
+
+    @GetMapping("/updateTeam")
+    public String updateTeam(Long teamID, Model model) {
+        model.addAttribute("title", "Update Team");
+        model.addAttribute("form", "/saveTeam");
+        GetTeamResponse response = teamClient.getTeamById(teamID);
+        TeamDTO teamDTO = response.getTeamDTO();
+        model.addAttribute("team", teamDTO);
+        return "team/teamForm";
+    }
+
 
 }
