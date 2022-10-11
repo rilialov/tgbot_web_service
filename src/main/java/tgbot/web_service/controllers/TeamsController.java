fcece0300f8119b9e23f67c4a5372c3b8c88df79
rilialov <ilialovrustem@gmail.com>
2022-10-11 15:30:01 +0300

    Changed Form and List pages for Team. Refactored TeamsController

--- src/main/java/tgbot/web_service/controllers/TeamsController.java
+++ src/main/java/tgbot/web_service/controllers/TeamsController.java
@@ -3,13 +3,16 @@ package tgbot.web_service.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import tgbot.users.service.GetAllTeamsResponse;
 import tgbot.users.service.GetTeamResponse;
 import tgbot.users.service.TeamDTO;
 import tgbot.web_service.service.TeamClient;
 
 @Controller
+@RequestMapping(value = "/teams")
 public class TeamsController {
 
     private final TeamClient teamClient;
@@ -18,38 +21,39 @@ public class TeamsController {
         this.teamClient = teamClient;
     }
 
-    @GetMapping("/teamsList")
+    @GetMapping
     public String teamsList(Model model) {
         GetAllTeamsResponse allTeams = teamClient.getAllTeams();
         model.addAttribute("teams", allTeams.getTeamsList());
         return "team/teamsList";
     }
 
-    @GetMapping("/createTeam")
+    @GetMapping("/new")
     public String createTeam(Model model) {
-        model.addAttribute("title", "Create Team");
-        model.addAttribute("form", "/saveNewTeam");
-        return "team/teamForm";
-    }
-
-    @PostMapping("/saveNewTeam")
-    public String saveNewTeam(String teamName, String teamColor) {
         TeamDTO teamDTO = new TeamDTO();
-        teamDTO.setTeamName(teamName);
-        teamDTO.setTeamColor(teamColor);
-        teamClient.saveTeam(teamDTO);
-        return "redirect:/teamsList";
-    }
-
-    @GetMapping("/updateTeam")
-    public String updateTeam(Long teamID, Model model) {
-        model.addAttribute("title", "Update Team");
-        model.addAttribute("form", "/saveTeam");
-        GetTeamResponse response = teamClient.getTeamById(teamID);
-        TeamDTO teamDTO = response.getTeamDTO();
+        model.addAttribute("title", "Create Team");
         model.addAttribute("team", teamDTO);
         return "team/teamForm";
     }
 
+    @GetMapping("/edit/{id}")
+    public String updateTeam(@PathVariable("id") Long teamID, Model model) {
+        GetTeamResponse response = teamClient.getTeamById(teamID);
+        TeamDTO teamDTO = response.getTeamDTO();
+        model.addAttribute("title", "Update Team");
+        model.addAttribute("team", teamDTO);
+        return "team/teamForm";
+    }
 
+    @PostMapping
+    public String saveTeam(TeamDTO teamDTO) {
+        teamClient.saveTeam(teamDTO);
+        return "redirect:/teams";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTeam(@PathVariable("id") Long teamID) {
+        teamClient.deleteTeamById(teamID);
+        return "redirect:/teams";
+    }
 }
