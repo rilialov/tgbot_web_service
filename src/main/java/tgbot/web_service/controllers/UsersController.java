b5e6a4b155d67ef820d1b8dd1c841bbbc7efec74
rilialov <ilialovrustem@gmail.com>
2022-11-17 15:22:51 +0300

    Added methods to UsersController added userForm and fixed taskForm and teamForm

--- src/main/java/tgbot/web_service/controllers/UsersController.java
+++ src/main/java/tgbot/web_service/controllers/UsersController.java
@@ -3,8 +3,12 @@ package tgbot.web_service.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tgbot.users.service.GetAllUsersResponse;
+import tgbot.users.service.GetUserResponse;
+import tgbot.users.service.UserDTO;
 import tgbot.web_service.service.UserClient;
 
 import java.util.Arrays;
@@ -40,4 +44,32 @@ public class UsersController {
         return "user/usersList";
     }
 
+    @GetMapping("/new")
+    public String createUser(Model model) {
+        UserDTO userDTO = new UserDTO();
+        model.addAttribute("title", "Create User");
+        model.addAttribute("user", userDTO);
+        return "user/userForm";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String updateUser(@PathVariable("id") Long userID, Model model) {
+        GetUserResponse response = userClient.getUserById(userID);
+        UserDTO userDTO = response.getUserDTO();
+        model.addAttribute("title", "Update User");
+        model.addAttribute("user", userDTO);
+        return "user/userForm";
+    }
+
+    @PostMapping
+    public String saveUser(UserDTO userDTO) {
+        userClient.saveUser(userDTO);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long userID) {
+        userClient.deleteUserById(userID);
+        return "redirect:/users";
+    }
 }
